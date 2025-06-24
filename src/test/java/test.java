df9869d7606953f42c72d4172610dbdb88a70cc1
ahmed <a.abdelsalam@wakeb.tech>
2025-06-24 16:59:33 +0300

    Initial commit wrhgffgfddfgfcgvbnt

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,8 +1,7 @@
-import org.junit.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.pag1;
-import static org.junit.Assert.assertNotNull;
+import static org.testng.AssertJUnit.assertNotNull;
 
 public class test extends base{
     @Test
